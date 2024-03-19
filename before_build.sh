8e32c6db429ea1666b3a7749e5e67351953ea9de
fata <ulhig@student.kit.edu>
2024-03-19 13:51:04 +0100

    cflags

--- before_build.sh
+++ before_build.sh
@@ -19,6 +19,7 @@ if [ "$(uname)" == "Darwin" ]; then
     sudo make install
     export LDFLAGS="-L/usr/local/lib" 
     export CPPFLAGS="-I/usr/local/include -std=c++11"
+    export CFLAGS="-I/usr/local/include -std=c++11"
 else
     make install
 fi
