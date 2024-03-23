d83e0f487ed47117a9f6cafed4643c10caf7824f
fata <ulhig@student.kit.edu>
2024-03-23 19:16:12 +0100

    fix, remove build-wheels script

--- before_build.sh
+++ before_build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e -u -x
 
-if [ "$(uname)" == "Linux" ]; then
+if [ "$(uname)" == "Linux" ] && [ $# -lt 1 ]; then
     cd /project
 fi
-
-# if [ "$(uname)" == "Darwin" ]; then
-#     brew install wget
-# fi
-
-# rm -rf gbd_test libarchive
 
 git clone https://github.com/libarchive/libarchive.git
 cd libarchive
@@ -30,20 +24,3 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release .. -DLA_PREFIX="/usr/local"  
 make -j4
 cd ..
-
-
-# mkdir gbd_test
-# cd gbd_test
-
-# mkdir benchmarks
-
-# wget --content-disposition -i scripts/allinstances.uri -P benchmarks
-
-# export GBD_DB=my.db
-# touch my.db
-# gbd init -j4 local benchmarks
-# gbd init -j4 base
-# gbd get -r ccs
-
-# cd ..
-# rm -rf gbd_test libarchive
