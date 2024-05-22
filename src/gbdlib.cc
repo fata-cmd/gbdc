bc3791b6c8fb0cace6c807d56b80a0d88545c264
fata <ulhig@student.kit.edu>
2024-05-22 14:31:20 +0200

    add cnf2kis, sanitize, version

--- src/gbdlib.cc
+++ src/gbdlib.cc
@@ -20,11 +20,15 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 #include <cstdio>
 #include <cstdint>
 #include <string>
+#include <chrono>
+#include <regex>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
 
 #include "src/identify/GBDHash.h"
 #include "src/identify/ISOHash.h"
 
-#include "src/util/ResourceLimits.h"
 #include "src/util/threadpool/ThreadPool.h"
 
 #include "src/extract/CNFBaseFeatures.h"
@@ -40,93 +44,55 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 
 namespace py = pybind11;
 
-// static std::uint16_t version(PyObject *self)
-// {
-//     return 1;
-// }
+static std::string version()
+{
+    std::ifstream file("setup.py");
+    if (!file.is_open())
+    {
+        return "Error: Could not open setup.py";
+    }
 
-// static PyObject *cnf2kis(PyObject *self, PyObject *arg)
-// {
-//     const char *filename;
-//     const char *output;
-//     unsigned maxEdges, maxNodes;
-//     unsigned rlim = 0, mlim = 0, flim = 0;
-//     PyArg_ParseTuple(arg, "ssII|III", &filename, &output, &maxEdges, &maxNodes, &rlim, &mlim, &flim);
+    std::string line;
+    std::regex version_regex(R"(version\s*=\s*['"]([^'"]+)['"])");
+    std::smatch match;
 
-//     PyObject *dict = pydict();
-//     pydict(dict, "nodes", 0);
-//     pydict(dict, "edges", 0);
-//     pydict(dict, "k", 0);
+    while (std::getline(file, line))
+    {
+        if (std::regex_search(line, match, version_regex))
+        {
+            return match[1].str();
+        }
+    }
 
-//     ResourceLimits limits(rlim, mlim, flim);
-//     limits.set_rlimits();
-//     try
-//     {
-//         IndependentSetFromCNF gen(filename);
-//         unsigned nNodes = gen.numNodes();
-//         unsigned nEdges = gen.numEdges();
-//         unsigned minK = gen.minK();
+    return "Error: Version not found in setup.py";
+}
 
-//         pydict(dict, "nodes", nNodes);
-//         pydict(dict, "edges", nEdges);
-//         pydict(dict, "k", minK);
+static auto cnf2kis(std::string filename, std::string output, size_t maxEdges, size_t maxNodes)
+{
+    std::unordered_map<std::string, std::string> dict;
 
-//         if ((maxEdges > 0 && nEdges > maxEdges) || (maxNodes > 0 && nNodes > maxNodes))
-//         {
-//             pydict(dict, "hash", "fileout");
-//             return dict;
-//         }
+    IndependentSetFromCNF gen(filename.c_str());
+    unsigned nNodes = gen.numNodes();
+    unsigned nEdges = gen.numEdges();
+    unsigned minK = gen.minK();
 
-//         gen.generate_independent_set_problem(output);
-//         pydict(dict, "local", output);
+    dict["nodes"] = std::to_string(nNodes);
+    dict["edges"] = std::to_string(nEdges);
+    dict["k"] = std::to_string(minK);
 
-//         std::string hash = CNF::gbdhash(output);
-//         pydict(dict, "hash", hash.c_str());
+    if ((maxEdges > 0 && nEdges > maxEdges) || (maxNodes > 0 && nNodes > maxNodes))
+    {
+        dict["hash"] = "fileout";
+        return dict;
+    }
 
-//         return dict;
-//     }
-//     catch (TimeLimitExceeded &e)
-//     {
-//         std::remove(output);
-//         pydict(dict, "hash", "timeout");
-//         return dict;
-//     }
-//     catch (MemoryLimitExceeded &e)
-//     {
-//         std::remove(output);
-//         pydict(dict, "hash", "memout");
-//         return dict;
-//     }
-//     catch (FileSizeLimitExceeded &e)
-//     {
-//         std::remove(output);
-//         pydict(dict, "hash", "fileout");
-//         return dict;
-//     }
-// }
+    gen.generate_independent_set_problem(output.c_str());
+    dict["local"] = output;
 
-// static PyObject *print_sanitized(PyObject *self, PyObject *arg)
-// {
-//     const char *filename;
-//     unsigned rlim = 0, mlim = 0;
-//     PyArg_ParseTuple(arg, "s|II", &filename, &rlim, &mlim);
-
-//     ResourceLimits limits(rlim, mlim);
-//     limits.set_rlimits();
-//     try
-//     {
-//         sanitize(filename);
-//         Py_RETURN_TRUE;
-//     }
-//     catch (TimeLimitExceeded &e)
-//     {
-//         Py_RETURN_FALSE;
-//     }
-//     catch (MemoryLimitExceeded &e)
-//     {
-//         Py_RETURN_FALSE;
-//     }
-// }
+    std::string hash = CNF::gbdhash(output.c_str());
+    dict["hash"] = hash.c_str();
+    return dict;
+}
 
 template <typename Extractor>
 static auto feature_names()
@@ -140,9 +106,10 @@ void bind_threadpool(py::module &m, const std::string &type_name)
 {
     py::class_<TP::ThreadPool<Extractor>>(m, type_name.c_str())
         .def(py::init<std::vector<std::string>, std::uint64_t, std::uint32_t>(),
+             "Thread pool used for parallel feature extraction. Takes a list of paths to CNF instances, the maximum amount of memory available and the maximum amount of parallel jobs permitted.",
              py::arg("paths"), py::arg("mem_max"), py::arg("jobs_max"))
-        .def("get_result_queue", &TP::ThreadPool<Extractor>::get_result_queue, "Returns ownership of the queue which is used to queue results from feature extraction.")
-        .def("start_threadpool", &TP::ThreadPool<Extractor>::start_threadpool, "Starts the threadpool. Has to be called after get_result_queue, otherwise extracted features can only be accessed after completion of all feature extractions.")
+        .def("get_result_queue", &TP::ThreadPool<Extractor>::get_result_queue, "Returns ownership of the queue which is used to output results from feature extraction.")
+        .def("start_threadpool", &TP::ThreadPool<Extractor>::start_threadpool, "Starts the threadpool. Has to be called after 'get_result_queue', otherwise extracted features can only be accessed after completion of all feature extractions.")
         .def("jobs_completed", &TP::ThreadPool<Extractor>::jobs_completed, "Returns true if all jobs have been completed, false otherwise.");
 }
 
@@ -152,23 +119,21 @@ PYBIND11_MODULE(gbdlib, m)
         .def(py::init<>())
         .def("pop", &MPSCQueue<TP::result_t>::pop, "Pop an element from the queue in a synchronized manner.")
         .def("empty", &MPSCQueue<TP::result_t>::empty, "Returns true if queue is empty, false otherwise.");
-    bind_threadpool<CNF::BaseFeatures>(m, "ExtractBaseFeatures");
-    bind_threadpool<CNFGateFeatures>(m, "ExtractGateFeatures");
-    bind_threadpool<OPB::BaseFeatures>(m, "ExtractOPBBaseFeatures");
-    bind_threadpool<WCNF::BaseFeatures>(m, "ExtractWCNFBaseFeatures");
+    bind_threadpool<CNF::BaseFeatures>(m, "extract_base_features");
+    bind_threadpool<CNFGateFeatures>(m, "extract_gate_features");
+    bind_threadpool<OPB::BaseFeatures>(m, "extract_opb_base_features");
+    bind_threadpool<WCNF::BaseFeatures>(m, "extract_wcnf_base_features");
+    m.def("version", &version, "Return current version of gbdc.");
+    m.def("cnf2kis", &cnf2kis, "Create k-ISP Instance from given CNF Instance.", py::arg("filename"), py::arg("output"), py::arg("maxEdges"), py::arg("maxNodes"));
+    m.def("sanitize", &sanitize, "Print sanitized, i.e., no duplicate literals in clauses and no tautologic clauses, CNF to stdout.", py::arg("filename"));
     m.def("base_feature_names", &feature_names<CNF::BaseFeatures>, "Get Base Feature Names");
     m.def("gate_feature_names", &feature_names<CNFGateFeatures>, "Get Gate Feature Names");
     m.def("wcnf_base_feature_names", &feature_names<WCNF::BaseFeatures>, "Get WCNF Base Feature Names");
     m.def("opb_base_feature_names", &feature_names<OPB::BaseFeatures>, "Get OPB Base Feature Names");
-    m.def("gbdhash", &CNF::gbdhash, "Calculates GBD-Hash (md5 of normalized file) of given DIMACS CNF file.");
+    m.def("gbdhash", &CNF::gbdhash, "Calculates GBD-Hash (md5 of normalized file) of given DIMACS CNF file.", py::arg("filename"));
     m.def("isohash", &WCNF::isohash, "Calculates ISO-Hash (md5 of sorted degree sequence) of given DIMACS CNF file.", py::arg("filename"));
-    m.def("opbhash", &OPB::gbdhash, "Calculates OPB-Hash (md5 of normalized file) of given OPB file.");
-    m.def("pqbfhash", &PQBF::gbdhash, "Calculates PQBF-Hash (md5 of normalized file) of given PQBF file.");
-    m.def("wcnfhash", &WCNF::gbdhash, "Calculates WCNF-Hash (md5 of normalized file) of given WCNF file.");
-    m.def("wcnfisohash", &WCNF::isohash, "Calculates WCNF ISO-Hash of given WCNF file.");
+    m.def("opbhash", &OPB::gbdhash, "Calculates OPB-Hash (md5 of normalized file) of given OPB file.", py::arg("filename"));
+    m.def("pqbfhash", &PQBF::gbdhash, "Calculates PQBF-Hash (md5 of normalized file) of given PQBF file.", py::arg("filename"));
+    m.def("wcnfhash", &WCNF::gbdhash, "Calculates WCNF-Hash (md5 of normalized file) of given WCNF file.", py::arg("filename"));
+    m.def("wcnfisohash", &WCNF::isohash, "Calculates WCNF ISO-Hash of given WCNF file.", py::arg("filename"));
 }
-
-// static PyMethodDef myMethods[] = {
-//     {"sanitize", print_sanitized, METH_VARARGS, "Print sanitized, i.e., no duplicate literals in clauses and no tautologic clauses, CNF to stdout."},
-//     {"cnf2kis", cnf2kis, METH_VARARGS, "Create k-ISP Instance from given CNF Instance."},
-// }
